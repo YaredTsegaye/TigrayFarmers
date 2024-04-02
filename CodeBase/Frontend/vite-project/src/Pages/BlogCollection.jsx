4ff11d44a09096a2ce2f73549601fcb080904da6
Yared.Tsegaye <yared.Tsegaye@ais.com>
2024-04-02 13:22:06 -0400

    update blogcollection with breadcrums

--- CodeBase/Frontend/vite-project/src/Pages/BlogCollection.jsx
+++ CodeBase/Frontend/vite-project/src/Pages/BlogCollection.jsx
@@ -1,5 +1,6 @@
 // BlogCollection.jsx
-import React from 'react';
+import React, {useEffect} from "react";
+import { useBreadcrumb } from './BreadcrumbContext';
 import BlogDetail from './BlogDetail';
 
 const blogs = [
@@ -15,24 +16,40 @@ const blogs = [
   // Include other blog entries here
 ];
 
-const BlogCollection = () => (
-  <div className="blog-section-two section-padding2" hidden={true}>
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-xl-7">
-          <div className="section-tittle text-center mb-50">
-            <span className="sub-tittle text-capitalize font-600">Blog</span>
-            <h2 className="title font-700">Our Blog & Feeds</h2>
+function BlogCollection() {
+  const { setBreadcrumbs } = useBreadcrumb();
+
+  useEffect(() => {
+    // Set the breadcrumb path for the BlogCollection page and make it visible
+    setBreadcrumbs({
+      items: [
+        { label: "Home", href: "/" },
+        { label: "Blog", href: "/BlogCollection" }
+      ],
+      isVisible: true
+    });
+  }, [setBreadcrumbs]);
+  return (
+    <>
+      <div className="blog-section-two section-padding2" hidden={true}>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-xl-7">
+              <div className="section-tittle text-center mb-50">
+                <span className="sub-tittle text-capitalize font-600">Blog</span>
+                <h2 className="title font-700">Our Blog & Feeds</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row gy-24">
+            {blogs.map((blog, index) => (
+              <BlogDetail key={index} {...blog} />
+            ))}
           </div>
         </div>
       </div>
-      <div className="row gy-24">
-        {blogs.map((blog, index) => (
-          <BlogDetail key={index} {...blog} />
-        ))}
-      </div>
-    </div>
-  </div>
-);
+    </>
+    );
+        }
 
 export default BlogCollection;
